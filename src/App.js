7963d469e20b1b345800c277a26177171bae6836
Temikiki <temitayokiki@gmail.com>
2025-08-07 02:23:01 +0100

    work in progrss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <main>
+    <div className='card'>
+      <img src="/assets/images/avatar-jessica.jpeg" alt=""></img>
+      <h1>jessica Randall</h1>
+      <h2>London, United Kindom</h2>
+      <p>"Front end developer and avid reader"</p>
+
+      <button>GitHub</button>
+      <button>Frontend Mentor</button>
+      <button>Linkedln</button>
+      <button>Twitter</button>
+      <button>Instagram</button>
     </div>
+
+    </main>
   );
 }
 
 export default App;
+
+ 
